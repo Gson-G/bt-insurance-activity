ea6904028829a6cf2eca567518f438b310413e4d
gantianming <gtm@cheok.com>
2019-03-12 10:20:54 +0800

    倒入excel pojo添加

---
+++ bt-insurance-activity-service/src/main/java/com/bz/ins/activity/util/ExcelPoji.java
@@ -0,0 +1,43 @@
+package com.bz.ins.activity.util;
+
+/**
+ * @author kantenmei
+ * @date 2019/3/11
+ * @time 11:05 AM
+ * @function 功能：
+ * @describe 版本描述：
+ * @modifyLog 修改日志：
+ */
+public class ExcelPoji {
+
+
+    private String name;
+
+    private String nickName;
+
+    private Boolean isBoss;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public Boolean getBoss() {
+        return isBoss;
+    }
+
+    public void setBoss(Boolean boss) {
+        isBoss = boss;
+    }
+}
